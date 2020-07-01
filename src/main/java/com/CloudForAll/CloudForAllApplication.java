06d8e64c17d431b436981a864984ce5ec96107a7
OscarRAmirez <oerm16@gmail.com>
2020-07-01 18:57:16 -0500

    inicio

---
+++ src/main/java/com/CloudForAll/CloudForAllApplication.java
@@ -0,0 +1,13 @@
+package com.CloudForAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudForAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudForAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CloudForAll/CloudForAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CloudForAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudForAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
